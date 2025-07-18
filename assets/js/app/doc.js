67f2cb56a041275ef6d8ddced307974bdc104932
mritter <mritter@ci-database.de>
2025-07-18 09:17:46 +0000

    tests

--- assets/js/app/doc.js
+++ assets/js/app/doc.js
@@ -1,33 +1,50 @@
- /*
-              $( document ).ready(function() {
-                  $(window).on('popstate', function() {
-                          loadContent(location.pathname);
-                  });
-  
-                  function loadContent(url) {
-                          $('#main').load(url + ' #main');
-                  }
-  
-                  $( ".ajax-call" ).click(function(event) {
-                          var This = this;
-                          var url = $(this).attr('href');
-                          event.preventDefault();
-                          $( "#content" ).load( url + ' #content', function(data, ret) {
-                             if (ret == 'success') {                 
-                                  $(".side-menu .ajax-call").removeClass( "active" );
-                                  $(This).addClass( "active" );
-  
-                                  history.pushState({}, "", url);
-  
-                             }
-                          });
-  
-  
-                    
-                        //  console.log($(this).attr('href'));
-                        //  $("#content").load($(this).attr('href') + ' #content');
-                          
-                         // console.log("etst");
-                    });
-              });
-  */
+
+
+function openUrl(url, success) {
+
+        
+        $('<body>').load(url, function (data, ret) {
+
+                if (ret == 'success') {
+
+
+                        $("#ci-page-content").html($("#ci-page-content", this).html())
+                        $("#navi").html($("#navi", this).html())
+                        $("#breadcrumb").html($("#breadcrumb", this).html())
+                        $("#col-aside").html($("#col-aside", this).html())
+                        //var pageInfo = {"id":this.id,"url":This.href};
+                        success(true)
+
+
+
+
+                } else {
+                        alert("error page loading");
+                        success(false)
+                }
+        });
+}
+
+window.addEventListener('popstate', function (e) {
+        if (e.state && e.state.ciHref) {
+                openUrl(e.state.ciHref, function success(ret) { })
+        }
+});
+
+
+jQuery(function () {
+        history.replaceState({ ciHref: window.location.href }, '', window.location.href);
+        $("#sidebar").on('click', '.ajax-call', function (event) {
+                event.preventDefault();
+                // var This = this;
+                var url = $(this).attr('href');
+
+                openUrl(url, function success(ret) {
+                        if (ret === true) {
+                                history.pushState({ ciHref: url }, '', url);
+                        }
+                })
+
+
+        });
+});
